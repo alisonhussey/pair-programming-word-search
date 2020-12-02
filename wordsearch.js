9a556424ba0a18b09414f3f8f1773c3f724b2968
alisonhussey <alisonhussey@gmail.com>
2020-12-02 22:45:43 +0000

    debugged code and added tests to check for vertical and undefined

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    let outcome = false;
+    if (letters.length <= 0) {
+        return outcome;
+    } else {
+        const horizontalJoin = letters.map(ls => ls.join(''));
+        let transposedArray = letters[0].map((x,i) => letters.map((x) => x[i]));
+        const verticalJoin = transposedArray.map(sl => sl.join(''));
+        for (l of horizontalJoin) {
+            if (l.includes(word)) {
+                outcome=true;
+            }
+        }
+        for (s of verticalJoin) {
+            if (s.includes(word)) {
+                outcome=true;
+            }
+        }
     }
-}
+    return outcome;
+};
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
